005f1e12b48f56d2d7f7e0beb25d7962a565c1b8
Peter Zandbergen <peter.zandbergen@myhops.com>
2023-09-26 17:50:20 +0200

    Do not use this

---
+++ start.go
@@ -0,0 +1,70 @@
+package neural
+
+import (
+	"math"
+	"math/rand"
+	"time"
+
+	"gonum.org/v1/gonum/mat"
+)
+
+type neuralNetConfig struct {
+	inputNeurons  int
+	outputNeurons int
+	hiddenNeurons int
+	learningRate  float64
+}
+
+type neuralNet struct {
+	config  neuralNetConfig
+	wHidden *mat.Dense
+	bHidden *mat.Dense
+	wOut    *mat.Dense
+	bOut    *mat.Dense
+}
+
+func newNetwork(config neuralNetConfig) *neuralNet {
+	return &neuralNet{
+		config: config,
+	}
+}
+
+func sigmoid(x float64) float64 {
+	return 1.0 / (1.0 + math.Exp(-x))
+}
+
+func sigmoidPrime(x float64) float64 {
+	return sigmoid(x) * (1.0 - sigmoid(x))
+}
+
+func (nn *neuralNet) train(x, y *mat.Dense) error {
+	// Initialize biases/weights.
+	randSource := rand.NewSource(time.Now().UnixNano())
+	randGen := rand.New(randSource)
+
+	wHidden := mat.NewDense(nn.config.inputNeurons, nn.config.hiddenNeurons, nil)
+	bHidden := mat.NewDense(1, nn.config.hiddenNeurons, nil)
+	wOut := mat.NewDense(nn.config.hiddenNeurons, nn.config.outputNeurons, nil)
+	bOut := mat.NewDense(1, nn.config.outputNeurons, nil)
+
+	wHiddenRaw := wHidden.RawMatrix().Data
+	bHiddenRaw := bHidden.RawMatrix().Data
+	wOutRaw := wOut.RawMatrix().Data
+	bOutRaw := bOut.RawMatrix().Data
+
+	for _, param := range [][]float64{
+		wHiddenRaw,
+		bHiddenRaw,
+		wOutRaw,
+		bOutRaw,
+	} {
+		for i := range param {
+			param[i] = randGen.Float64()
+		}
+	}
+
+	output := new(mat.Dense)
+
+	
+	return nil
+}
